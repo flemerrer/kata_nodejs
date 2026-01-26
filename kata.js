f960b091fd6427daf1a85861821400d74b83c7fb
francois <francois.lemerrer@pandata.ninja>
2026-01-26 10:05:50 +0100

    init project

---
+++ kata.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+	response.send('Hello World!')
+})
+
+app.listen(3000, () => {
+	console.log('Server started on port 3000!')
+})
